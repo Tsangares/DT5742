2100aa37a7e5ca76708449bde707b0bf764429b8
William Wyatt <tsangares@gmail.com>
2019-07-11 17:37:08 -0700

    Updated parser script.

---
+++ parseDigitizer.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from numpy import fromfile,dtype
+
+
+def parseBinary(filename):
+    with open(filename, mode='rb') as f:
+        rawBinary=fromfile(f,dtype=dtype("<f"),count=-1)
+        lenEvent=1030 #Length of an event
+        lenHeader=6   #Length of the header info
+        nEvents=len(rawBinary)//lenEvent
+        
+        #Seperate traces
+        traces=[ [float(adc) for adc in rawBinary[i*lenEvent+lenHeader:(i+1)*lenEvent]] for i in range(nEvents) ]
+        return traces
+
+def plotEvent(data,event=1):
+    plt.plot(range(len(data[event])),data[event],linestyle="None",marker=".")
+    plt.show()
+
+if __name__=='__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Parse and view the output data from the Caen Digitizer DT5742.')
+    parser.add_argument('filename', type=str, help='The path to a single data file produced by the digitizer.')
+    parser.add_argument('-e','--event', metavar='event', type=int, help='The index of the event you want returned. If you do not enable this it will return all the events.',default=0)
+    args = parser.parse_args()
+    print("Opening",args.filename)
+    plotEvent(parseBinary(args.filename),args.event)
+    
+    
