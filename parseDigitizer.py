682270b800b85ce07dd246b672f0798cb5f3d291
William Wyatt <tsangares@gmail.com>
2019-07-11 17:40:07 -0700

    Updated parser script.

--- parseDigitizer.py
+++ parseDigitizer.py
@@ -1,3 +1,4 @@
+#!/
 import matplotlib.pyplot as plt
 from numpy import fromfile,dtype
 
@@ -17,6 +18,13 @@ def plotEvent(data,event=1):
     plt.plot(range(len(data[event])),data[event],linestyle="None",marker=".")
     plt.show()
 
+def extractChannel(filename):
+    try:
+        return int(filename[-7:-4].replace('e','').replace('_',''))
+    except:
+        print("Could not parse the channel number")
+        return None
+        
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Parse and view the output data from the Caen Digitizer DT5742.')
@@ -24,6 +32,10 @@ if __name__=='__main__':
     parser.add_argument('-e','--event', metavar='event', type=int, help='The index of the event you want returned. If you do not enable this it will return all the events.',default=0)
     args = parser.parse_args()
     print("Opening",args.filename)
-    plotEvent(parseBinary(args.filename),args.event)
+    events=parseBinary(args.filename)
+    chan=extractChannel(args.filename)
+    print('This is channel {} and there are {} events.'.format(chan,len(events)))
+    print("Plotting event",args.event)
+    plotEvent(events,args.event)
     
     
